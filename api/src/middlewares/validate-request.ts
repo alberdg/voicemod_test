1f0bb43d4bbfaac8280ba6b195bed63e1083d8e0
alberdg811 <albertogodar81@gmail.com>
2020-09-16 23:29:03 +0200

    Tests and implementation for signup route.
    - Implemented middleware to validate requests using express validator

---
+++ api/src/middlewares/validate-request.ts
@@ -0,0 +1,23 @@
+import { Request, Response, NextFunction } from 'express';
+import { validationResult } from 'express-validator';
+import { RequestValidationError } from '../errors/request-validation-error';
+
+/**
+ * Middleware function to validate requests
+ * @function
+ * @param req Request object
+ * @param res Response object
+ * @param next Next callback
+ */
+export const validateRequest = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    throw new RequestValidationError(errors.array());
+  }
+
+  next();
+};
