30d7e69f3c6944ebfbbfa9284893152cadb351f5
alberdg811 <albertogodar81@gmail.com>
2020-09-16 12:53:26 +0200

    Implemented mongodb connection and mongodb termination

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose';
 import { app } from './app';
 const HTTP_PORT = 3001;
 
@@ -6,9 +7,30 @@ const HTTP_PORT = 3001;
  * @function
  */
 const start = async () => {
+  // Throw an error if mongo uri env variable does not exist
+  if (!process.env.MONGO_URI) {
+    throw new Error('MONGO_URI must be defined');
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    });
+  } catch (err) {
+    console.error(err);
+  }
+
   app.listen(HTTP_PORT, () => {
     console.log(`Listening on port ${HTTP_PORT}`);
   });
 };
 
 start();
+
+// Process terminated time to clean up
+process.on('SIGTERM', () => mongoose.connection.close());
+
+// Process terminated time to clean up
+process.on('SIGINT', () => mongoose.connection.close());
