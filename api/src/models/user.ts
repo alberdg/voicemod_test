28630b94001f4d7f1dc8e86e708f9480d7de597e
alberdg811 <albertogodar81@gmail.com>
2020-09-16 11:38:52 +0200

    User model implementation, Password hashing is pending

---
+++ api/src/models/user.ts
@@ -0,0 +1,96 @@
+import mongoose from 'mongoose';
+
+
+// An interface that describes the properties
+// that are required to create a new User
+interface UserAttrs {
+  name: string;
+  lastname: string;
+  email: string;
+  password: string;
+  country: string;
+  telephone: string;
+  postcode: string;
+}
+
+// An interface that describes the properties
+// that a User Model has
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc;
+}
+
+// An interface that describes the properties
+// that a User Document has
+interface UserDoc extends mongoose.Document {
+  name: string;
+  lastname: string;
+  email: string;
+  password: string;
+  country: string;
+  telephone: string;
+  postcode: string;
+}
+
+// User schema to be exposed
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    lastname: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      required: true
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    country: {
+      type: String,
+      required: true
+    },
+    telephone: {
+      type: String,
+      required: true
+    },
+    postcode: {
+      type: String,
+      required: true
+    }
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        /**
+         * Transforming _id to id as a best practice this is not the case
+         * but this system could be part of a bigger system using other databases
+         * different from mongodb where _id is not the standard naming
+         */
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.password; // Removing password for enhanced security
+        delete ret.__v; // Very specific to mongodb
+      }
+    }
+  }
+);
+
+userSchema.pre('save', async function(done) {
+  if (this.isModified('password')) {
+    //TODO Need to hash the user password before saving it
+  }
+  done();
+});
+
+userSchema.statics.build = (attrs: UserAttrs) => {
+  return new User(attrs);
+};
+
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
+
+export { User };
