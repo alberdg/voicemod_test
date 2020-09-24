47268f9a0afbed90a336d81dd327f2dd4c3253ea
alberdg811 <albertogodar81@gmail.com>
2020-09-24 11:32:59 +0200

    Final cleanup

--- api/src/routes/country.ts
+++ api/src/routes/country.ts
@@ -1,7 +1,6 @@
 import { Router, Request, Response } from 'express';
 import { Country } from '../models/country';
 import { redisWrapper } from '../redis/redis-wrapper';
-import { COUNTRIES } from '../constants';
 const COUNTRIES_KEY = 'countries';
 const countryRouter = Router();
 
@@ -13,11 +12,6 @@ const cacheCountries = async (): Promise<void> => {
   const countries = await Country.find({});
   redisWrapper.redisSet(COUNTRIES_KEY, countries);
 }
-
-countryRouter.get('/api/countries/add', async (req: Request, res: Response) => {
-  COUNTRIES.map((country: any) => new Country({ name: country }).save());
-  res.send({});
-})
 
 /**
  * Fetches all countries
